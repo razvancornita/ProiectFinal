9578dcdb6e5ea9487eb6b59ae6a35601a912c161
razvancornita <cornita_razvan@yahoo.co>
2020-09-09 19:49:58 +0300

    am adaugat fisierele de baza

---
+++ Pisica.java
@@ -0,0 +1,45 @@
+
+
+public class Pisica {
+    private int varsta;
+    private String nume;
+    private String rasa;
+    private String culoare;
+
+    private Pisica() {
+
+    }
+
+    private Pisica(int varsta, String nume, String rasa, String culoare) {
+        this.varsta = varsta;
+        this.nume = nume;
+        this.rasa = rasa;
+        this.culoare = culoare;
+    }
+
+    private void spuneVarsta() {
+        System.out.println("Am " + this.varsta + " ani :)");
+    }
+
+    private void spuneMiau() {
+        System.out.println("Miau!");
+    }
+
+    private void spuneNume() {
+        System.out.println("Numele meu este " + this.nume);
+    }
+
+    private void salutaOm(String numeOm) {
+        System.out.println("Salut, " + numeOm);
+    }
+
+    @Override
+    public String toString() {
+        return "Pisica{" +
+                "varsta=" + varsta +
+                ", nume='" + nume + '\'' +
+                ", rasa='" + rasa + '\'' +
+                ", culoare='" + culoare + '\'' +
+                '}';
+    }
+}
